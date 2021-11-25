957495f18ec4e867cc0f78278d53aa8f438a565a
john <t69685186@gmail.com>
2021-11-25 21:42:35 +0800

    py爬蟲抓標題

--- 練習/p.py
+++ 練習/p.py
@@ -1,1 +1,11 @@
-print(1230)
+import urllib.request as req
+# 抓取網頁原始碼
+url="https://www.ptt.cc/bbs/movie/index.html"
+request=req.Request(url, headers={
+  "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+})
+with req.urlopen(request) as response:
+  data=response.read().decode("utf-8")
+  import bs4
+  root=bs4.BeautifulSoup(data, "html.parser")
+  print(root.title.string)
