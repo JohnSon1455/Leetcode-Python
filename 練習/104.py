af4c8a60402f3d0f9a39879048263d17fc7b6f33
john <t69685186@gmail.com>
2021-11-30 23:39:21 +0800

    抓單一標題

--- 練習/104.py
+++ 練習/104.py
@@ -1,21 +1,22 @@
-import urllib.request as req
-# 抓取網頁原始碼
-def getData(url):
-  request=req.Request(url, headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-  })
-  with req.urlopen(request) as response:
-    data=response.read().decode("utf-8")
-    import bs4
-    root=bs4.BeautifulSoup(data, "html.parser")
-    titles=root.find_all("div", class_="title")
-    for title in titles:
-      if title.a != "None":
-        print(title.a.string)
-    nextlink=root.find("a", string="‹ 上頁")
-    return nextlink["href"]
-pageURL="https://www.104.com.tw/jobs/main/"
-count=0
-while count<3:
-  pageURL="https://www.104.com.tw/jobs/main/"+getData(pageURL)
-  count+=1
+import requests
+
+
+def get_job(job_id):
+    """取得職缺詳細資料"""
+    url = f'https://www.104.com.tw/job/ajax/content/{job_id}'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+        'Referer': f'https://www.104.com.tw/job/{job_id}'
+    }
+
+    r = requests.get(url, headers=headers)
+    if r.status_code != requests.codes.ok:
+        print('請求失敗', r.status_code)
+        return
+
+    data = r.json()
+    return data['data']
+
+
+job_info = get_job('71gqf')
+print(job_info)
